bdf8e3af89c71ba413d47e78a0572143ad572f86
phannhat <phannhat2609@gmail.com>
2022-06-30 18:05:10 +0900

    /upload code

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hello em ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'phải chăng em cũng thích a, cho a cơ hội làm người yêu em nhá',
+    desc: 'Em không trả lời hoặc thoát ra, là em đồng ý đó nha :> ',
+    btnYes: 'Có,..em thật sự thích anh',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'hỏi nhiều thế,lo làm cho tốt đi',
+    mess: 'vâng ạ. đã rõ 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/phan.nhat.75436531'
+}
